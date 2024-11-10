30fa91447af56890def8e1013f90a865bc5e7b66
JunpeiAbe <uihrsw0228@gmail.com>
2024-11-10 15:10:15 +0900

    APIProtocols add

--- Sources/APIUnitTestLearn/APIProtocols.swift
+++ Sources/APIUnitTestLearn/APIProtocols.swift
@@ -1,27 +1,32 @@
+import Foundation
 
-
+/// APIクライアントの定義
 protocol APIClientProtocol {
     func request<Request: APIRequestable>(_ request: Request) async throws -> Request.Response
 }
-
+/// APIリクエストの定義
 protocol APIRequestable: Encodable {
     associatedtype Response: APIResponsable
-    var baseURL: URL { get set }
-    var urlSuffix: String { get set }
+    var baseURL: URL { get }
+    var path: String { get }
     var httpMethod: HttpMethod { get }
     var httpHeaders: HttpHeaders? { get }
-    var httpBody: HttpBody? { get set }
+    var httpBody: HttpBody? { get }
 }
 
-protocol HttpBody: Encodable {}
-
+/// APIレスポンスの定義
 protocol APIResponsable: Decodable {}
 
+/// HTTPメソッド
 enum HttpMethod: String {
-    case get
-    case post
+    case get = "GET"
+    case post = "POST"
 }
-
+/// HTTPヘッダー
 struct HttpHeaders {
     var headers: [String : String] = [:]
+}
+/// HTTPボディ
+struct HttpBody: Encodable {
+    /// リクエストデータを定義
 }
